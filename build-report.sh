1abd78d2a04c8642681c54ef96bffc28a8ab7fd2
Orhan Obut <orhanobut@gmail.com>
2016-08-12 14:47:26 +0200

    Update build-report.sh

--- build-report.sh
+++ build-report.sh
@@ -7,7 +7,7 @@ mkdir app/build/report
 mkdir app/build/report/current
 mkdir app/build/report/new
 
-PR_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+PR_BRANCH=$(git rev-parse --short HEAD)
 echo "PR BRANCH=$PR_BRANCH"
 
 MERGE_BRANCH=$(git log --merges --grep "Merge pull request" -1 --format=format:%h)
@@ -15,14 +15,23 @@ echo "MERGE BRANCH=$MERGE_BRANCH"
 
 # build apk from master and fetch apk info
 git checkout $MERGE_BRANCH
+echo "Current  branch = $MERGE_BRANCH"
+echo "Build current apk"
 ./gradlew assembleRelease
+echo "Copy current apk"
 cp app/build/outputs/apk/app-release.apk app/build/report/current/app-release.apk
+echo "Unzipping current apk"
 unzip app/build/report/current/app-release.apk -d app/build/report/current > /dev/null
 
 # switch back to PR branch
 git checkout $PR_BRANCH
+echo "Current branch = $PR_BRANCH"
+echo "Build new apk"
 ./gradlew assembleRelease
+echo "Copy new apk"
 cp app/build/outputs/apk/app-release.apk app/build/report/new/app-release.apk
+echo "Unzipping new apk"
 unzip app/build/report/new/app-release.apk -d app/build/report/new > /dev/null
 
+echo "Build report"
 python app/build/report/build_report.py
