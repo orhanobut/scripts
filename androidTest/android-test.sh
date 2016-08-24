4337cc1ba51b989259013f494eada1f7a91719dd
orhanobut <orhanobut@gmail.com>
2016-08-24 21:11:13 +0200

    Refactor android-test

--- androidTest/android-test.sh
+++ androidTest/android-test.sh
@@ -1,50 +1,62 @@
 #!/usr/bin/env bash
 
-if [ -z $1 ]
-then
-  echo "Missing applicationId, ie: $ sh android-test.sh <APPLICATION_ID> <PACKAGE UNDER TEST> <RUNNER>"
+PACKAGE_NAME=""
+RUNNER=""
+APP_ID=""
+
+while (( "$#" )); do
+  case "$1" in 
+  "--package") 
+     shift
+     PACKAGE_NAME=$1
+     ;;
+  "--app-id")
+     shift
+     APP_ID=$1
+     ;;
+  "--runner")
+     shift
+     RUNNER=$1
+     ;;
+  esac
+  shift
+done
+
+if [[ ( $PACKAGE_NAME == "" ) || ( $APP_ID == "" ) || ( $RUNNER == "") ]]
+  then
+    echo "Required parameters are missing, --package, --app-id and --runner must be set"
+    exit 1
 fi
 
-APPLICATION_ID=$1
-
-if [ -z $2 ]
-then
-  echo "Missing package name. ie: $ sh android-test.sh <APPLICATION_ID> <PACKAGE UNDER TEST> <RUNNER>"
-  exit 1
-fi
-
-PACKAGE_NAME_UNDER_TEST=$2
-
-if [ -z $3 ]
-then
-  echo "Missing runner parameter. ie: $ sh android-test.sh <APPLICATION_ID> <PACKAGE UNDER TEST> <RUNNER>"
-fi
-
-RUNNER=$3
-
-DEBUG_APK="app/buildss/outputs/apk/app-debug.apk"
-if [ ! -e "DEBUG_APK" ]
-then
-  ./gradlew assembleDebug
-fi
+echo "Package name under test = $PACKAGE_NAME"
+echo "App id under test = $APP_ID"
+echo "Runner for tests = $RUNNER"
 
 # Installing debug apk
-adb push app/build/outputs/apk/app-debug.apk "/data/local/tmp/$APPLICATION_ID"
-adb shell pm install -r "/data/local/tmp/$APPLICATION_ID"
+adb push app/build/outputs/apk/app-debug.apk "/data/local/tmp/$APP_ID"
+adb shell pm install -r "/data/local/tmp/$APP_ID"
 
 # Installing test apk
-./gradlew assembleAndroidTest
-adb push app/build/outputs/apk/app-debug-androidTest-unaligned.apk "/data/local/tmp/$APPLICATION_ID.test"
-adb shell pm install -r "/data/local/tmp/$APPLICATION_ID.test"
+./gradlew assembleAndroidTest >> /dev/null
+adb push app/build/outputs/apk/app-debug-androidTest-unaligned.apk "/data/local/tmp/$APP_ID.test"
+adb shell pm install -r "/data/local/tmp/$APP_ID.test"
 
 # Running tests
-adb shell am instrument -w -r -e package "$PACKAGE_NAME_UNDER_TEST" -e debug false "$APPLICATION_ID.test/$RUNNER" > android-test-log.txt
+adb shell am instrument -w -r -e package "$PACKAGE_NAME" -e debug false "$APP_ID.test/$RUNNER" > android-test-log.txt
 
 cat android-test-log.txt
 
-if grep "Failures" android-test-log.txt
+# Generate report
+REPORT_FILE=$(echo $PACKAGE_NAME | tr '.' '_').txt
+
+grep "Time" android-test-log.txt >> $REPORT_FILE
+grep -e "OK (" android-test-log.txt >> $REPORT_FILE
+grep -e "Tests run" android-test-log.txt >> $REPORT_FILE
+sed -n "s/INSTRUMENTATION_STATUS: class=//gp" android-test-log.txt | uniq >> $REPORT_FILE
+
+if grep "OK (" android-test-log.txt
 then
-  echo "FUNCTIONAL TESTS FAILED"
-else
   echo "FUNCTIONAL TESTS SUCCESS"
+else
+  echo "FUNCTIONAL TESTS FAILED"
 fi
